1073bfcd02e1eb015fa084906420d33be53d36e4
Milind-Ranjan <milindranjan1511@gmail.com>
2025-06-23 00:32:36 +0530

    Switch to static generation to avoid native binding issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
+  ssr: false,
   nitro: {
-    preset: 'cloudflare-pages',
-    experimental: {
-      wasm: true
+    prerender: {
+      routes: ['/']
     }
   },
   modules: [
